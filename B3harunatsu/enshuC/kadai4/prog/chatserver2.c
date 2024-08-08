24e2c0b0d6970b56b08ad3588be09c73072b1db4
09B22084 <09B22084@localhost>
2024-08-08 11:32:45 +0900

    sample

--- B3harunatsu/enshuC/kadai4/prog/chatserver2.c
+++ B3harunatsu/enshuC/kadai4/prog/chatserver2.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 
 #define MAXCLIENTS 5
 
@@ -14,26 +14,23 @@ int main(){
     int sock,n,k,sd,clen,max_sd,new_socket,new_socket_num,username_flag;
     int csock[5];
     char registered_username[MAXCLIENTS][256];
+    char client_ip[MAXCLIENTS][INET_ADDRSTRLEN];
     struct sockaddr_in svr;
     struct sockaddr_in clt;
     fd_set rfds;
     struct timeval tv;
     char rbuf[1024];
-    char message[1024];
+    char name_message[1024];
     char accept_request_message[] = "REQUEST ACCEPTED\n";
     char reject_request_message[] = "REQUEST REJECTED\n";
     char accept_username_message[] = "USERNAME REGISTERED\n";
     char reject_username_message[] = "USERNAME REJECTED\n";
-
-    time_t current_time;
-    struct tm *local_time;
-    int year,month,day,hour,min,sec;
-
+    
+    /*状態1*/
     for (int i = 0; i < MAXCLIENTS; i++) {
         csock[i] = -1;
         bzero(registered_username[i],256);
     }
-    /*状態1*/
     if ((sock=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP))<0) {
         perror("socket");
         exit(1);
@@ -69,10 +66,8 @@ int main(){
             }
         }
         if (select(max_sd + 1, &rfds, NULL, NULL, &tv) > 0) {
-            printf("state3\n");
             /*状態3*/
             if (FD_ISSET(sock, &rfds)) {/*状態4*/
-                printf("state4\n");
                 clen = sizeof(clt);
                 if ((new_socket = accept(sock, (struct sockaddr *)&clt, &clen)) < 0) {
                     perror("accept");
@@ -85,6 +80,8 @@ int main(){
                         close(new_socket);
                         break;
                     }
+                    
+                    /*状態5*/
                     for (int i = 0; i < MAXCLIENTS; i++) {//登録可能なソケット番号を確認
                         if (csock[i] == -1) {
                             new_socket_num = i;
@@ -92,43 +89,52 @@ int main(){
                             break;
                         }
                     }
-                    /*状態5*/
-                    printf("state5\n");
+                    
                     bzero(rbuf, 1024);
                     n = read(new_socket, rbuf, 1024);//登録するユーザー名を取得
                     if(n <= 0){
                         perror("Connection closed by client.\n");
                         close(new_socket);
                         return 0;
-                    }else{
-                        username_flag = 0;
-                        for(int i=0;i<MAXCLIENTS;i++){//登録するユーザー名が既に使用されていないかを確認
-                            if (strcmp(registered_username[i], rbuf) == 0) {
-                                username_flag = 1;
-                                break;
-                            }
+                    }
+                    username_flag = 0;
+                    for(int i=0;i<1024;i++){
+                        if(rbuf[i] == '\n'){
+                            rbuf[i] = '\0';
+                            break;
                         }
-                        if(username_flag == 0){
-                            n = write(new_socket, accept_username_message, strlen(accept_username_message));
-                            if(n < 0){
-                                perror("ERROR writing");
-                                close(new_socket);
-                                break;
-                            }
-                            strcpy(registered_username[new_socket_num], rbuf);
-                            printf("%s is registered\n",registered_username[new_socket_num]);
-                            csock[new_socket_num] = new_socket;
-                            k++;
-                        }else{
-                            n = write(new_socket, reject_username_message, strlen(reject_username_message));
-                            if(n < 0){
-                                perror("ERROR writing");
-                                close(new_socket);
-                                break;
-                            }
-                            csock[new_socket_num] = -1;
+                        if(!isalnum(rbuf[i]) && rbuf[i] != '_' && rbuf[i] != '_'){
+                            username_flag = 1;
+                        }
+                    }
+                    for(int i=0;i<MAXCLIENTS;i++){//登録するユーザー名が既に使用されていないかを確認
+                        if (strcmp(registered_username[i], rbuf) == 0) {
+                            username_flag = 1;
+                            break;
+                        }
+                    }
+                    if(username_flag == 0){
+                        n = write(new_socket, accept_username_message, strlen(accept_username_message));
+                        if(n < 0){
+                            perror("ERROR writing");
                             close(new_socket);
+                            break;
                         }
+                        inet_ntop(AF_INET,&clt.sin_addr, client_ip[new_socket_num], INET_ADDRSTRLEN);
+                        strcpy(registered_username[new_socket_num], rbuf);
+                        printf("%s\n",client_ip[new_socket_num]);
+                        printf("%s is registered\n",registered_username[new_socket_num]);
+                        csock[new_socket_num] = new_socket;
+                        k++;
+                    }else{
+                        n = write(new_socket, reject_username_message, strlen(reject_username_message));
+                        if(n < 0){
+                            perror("ERROR writing");
+                            close(new_socket);
+                            break;
+                        }
+                        csock[new_socket_num] = -1;
+                        close(new_socket);
                     }
                 }else{
                     n = write(new_socket, reject_request_message, strlen(reject_request_message));
@@ -142,43 +148,22 @@ int main(){
             }
             for(int i=0;i<MAXCLIENTS;i++){
                 if ((csock[i] != -1) && (FD_ISSET(csock[i], &rfds))){/*状態6*/
-                    
-                    printf("state6\n");
                     //ソケットから受信したなら
                     bzero(rbuf, 1024);
                     n = read(csock[i], rbuf, 1024);
                     if(n <= 0){/*状態7*/
-                        printf("state7\n");
                         close(csock[i]);
                         printf("%s has left the chat room\n",registered_username[i]);
                         k--;
                         csock[i] = -1;
+                        bzeo(client_ip[i],INET_ADDRSTRLEN);
                         bzero(registered_username[i],256);
                         break;
                     }else{
-                        //変更点1
-                        time(&current_time);
-                        local_time = localtime(&current_time);
-                        year = local_time->tm_year + 1900;
-                        month = local_time->tm_mon + 1;
-                        day = local_time->tm_mday;
-                        hour = local_time->tm_hour;
-                        min = local_time->tm_min;
-                        sec = local_time->tm_sec;
-
-                        if(strcmp(rbuf,"/list\n") == 0){
-                            for(int j=0;j<MAXCLIENTS;j++){
-                                if(csock[j] != -1){
-                                    snprintf(message, sizeof(message), "%s \n%s", registered_username[j], message);
-                                }
-                            }
-                            snprintf(message,sizeof(message),"%s\n",message);
-                        }else{
-                            snprintf(message, sizeof(message), ">%s[%2d:%2d] %s", registered_username[i],hour , min , rbuf);
-                        }
                         for(int j=0;j<MAXCLIENTS;j++){
                             if(csock[j] != -1){
-                                n = write(csock[j], message, strlen(message));
+                                snprintf(name_message, sizeof(name_message), ">%s %s", registered_username[i], rbuf);
+                                n = write(csock[j], name_message, strlen(name_message));
                                 if(n < 0){
                                     perror("ERROR writing");
                                     break;
